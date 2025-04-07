4baa45b0ed1e75e598113ec8f9117b45f1dc9a7b
maia <maianeto2014@gmail.com>
2025-04-07 11:32:16 -0300

    Added the Service layer and a PUT method

--- src/main/java/com/spring/project/controller/RunController.java
+++ src/main/java/com/spring/project/controller/RunController.java
@@ -1,7 +1,6 @@
 package com.spring.project.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,51 +8,46 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.project.model.Run;
-import com.spring.project.model.RunNotFoundException;
-import com.spring.project.repository.RunRepository;
+import com.spring.project.service.RunService;
 
 @RestController
 @RequestMapping("/api/runs")
 public class RunController {
     @Autowired
-    private RunRepository runRepository;
+    private RunService runService;
 
     @GetMapping(value = "/")
     public List<Run> findaAll() {
-        return runRepository.findAll();
+        return runService.findaAllRuns();
     }
 
     @GetMapping(value = "/{id}")
     public Run findById(@PathVariable Long id) {
-        Optional<Run> run = runRepository.findById(id);
-        if(run.isEmpty()) {
-            throw new RunNotFoundException();
-        }
-        return run.get();
+        return runService.findRunById(id);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/")
     public void save(@RequestBody Run run) {
-        runRepository.save(run);
+        runService.createRun(run);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping(value = "/{id}")
+    public void update(@RequestBody Run run, @PathVariable Long id) {
+        runService.updateRun(run, id);
+    }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping(value = "/{id}")
     public void delete(@PathVariable Long id){
-        Optional<Run> run = runRepository.findById(id);
-        if(run.isEmpty()) {
-            throw new RunNotFoundException();
-        }
-
-        runRepository.delete(run.get());
+        runService.deleteRun(id);
     }
-
 }
--- src/main/java/com/spring/project/service/RunService.java
+++ src/main/java/com/spring/project/service/RunService.java
@@ -1,11 +1,13 @@
 package com.spring.project.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.spring.project.model.Run;
+import com.spring.project.model.RunNotFoundException;
 import com.spring.project.repository.RunRepository;
 
 @Service
@@ -14,4 +16,37 @@ public class RunService {
     @Autowired
     private RunRepository runRepository;
 
+    public List<Run> findaAllRuns() {
+        return runRepository.findAll();
+    }
+
+    public Run findRunById(Long id) {
+        Optional<Run> run = runRepository.findById(id);
+        if(run.isEmpty()) {
+            throw new RunNotFoundException();
+        }
+        return run.get();
+    }
+
+    public void createRun(Run run) {
+        runRepository.save(run);
+    }
+
+    public void updateRun(Run run, Long id) {
+        Run updatedRun = findRunById(id);
+
+        updatedRun.setTitle(run.getTitle());
+        updatedRun.setStartedOn(run.getStartedOn());
+        updatedRun.setCompletedOn(run.getCompletedOn());
+        updatedRun.setMiles(run.getMiles());
+        updatedRun.setLocation(run.getLocation());
+
+        runRepository.save(updatedRun);
+    }
+
+    public void deleteRun(Long id) {
+        Run run = findRunById(id);
+
+        runRepository.delete(run);
+    }
 }
